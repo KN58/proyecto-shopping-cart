d8502b3e795734f061f8fe64de318ace38eda806
Majo <mjpazmino6@espe.edu.ec>
2020-09-08 19:30:15 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+//feature-1
+import React from 'react';
+
+function App() {
+  return (
+<div className="grid-container">
+  <header>
+    <a href="/">Carrito de compras React</a>
+  </header>
+  <main>Lista de productos</main>
+  <footer>Todos los derechos estan reservados</footer>
+</div>
+  );
+}
+
+export default App;
